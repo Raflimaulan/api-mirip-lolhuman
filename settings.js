f7f1ba07dc032a2855a66f17714fc443a2adc6f0
Raflimaulan <119143416+Raflimaulan@users.noreply.github.com>
2022-12-28 04:01:44 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'RaffX Teams' 
+global.apikey = ["RaffXTeams", "Raffx", "raffxteamsid"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
